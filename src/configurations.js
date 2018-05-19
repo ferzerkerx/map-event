acbf6433dfb18e62534d55ce478a8b371be12016
fernandoMontes <fernando@element.in>
2018-05-19 14:18:59 +0200

    - cleanup

---
+++ src/configurations.js
@@ -0,0 +1,67 @@
+import * as d3 from 'd3'
+import * as topojson from 'topojson'
+
+const defaultConfig = {
+  mapUrl: 'http://localhost:8080/json/mex0.json',
+  mapDomElement: 'map',
+  toolTipDomElement: 'tooltip',
+  height: 768,
+  width: 1024,
+  projection: (width, height) => {
+    return d3
+      .geoMercator()
+      .scale(defaultConfig.scale)
+      .translate([width / 2, height / 2]);
+  },
+  zoomEnabled: false,
+  scale: 3300,
+  featureExtractor: mapData => {
+    return topojson.feature(mapData, mapData.objects.state_data).features;
+  },
+
+  regionDisplayDataExtractor: regionData => {
+    return `${regionData.properties.NAME_1} <br/>`;
+  },
+  regionClassMapper: regionData => {
+    const regionName = regionData.properties.NAME_1;
+    return regionName.replace(/\s+/g, '_').toLowerCase();
+  }
+};
+
+const deConfig = Object.assign({}, defaultConfig, {
+  mapUrl: 'http://localhost:8080/json/de0.json',
+  projection: (width, height) => {
+    return d3
+      .geoMercator()
+      .scale(defaultConfig.scale)
+      .translate([-60, height * 5]);
+  }});
+
+const frConfig = Object.assign({}, defaultConfig, {
+  mapUrl: 'http://localhost:8080/json/fra0.json',
+  projection: (width, height) => {
+    return d3
+      .geoMercator()
+      .scale(defaultConfig.scale)
+      .translate([width / 2, height * 4.5]);
+  }});
+
+const mxConfig = Object.assign({}, defaultConfig, {
+  mapUrl: 'http://localhost:8080/json/mex0.json',
+  scale: 2000,
+  projection: (width, height) => {
+    return d3
+      .geoAlbers()
+      .scale(mxConfig.scale)
+      .translate([width / 1.5, -100]);
+  }});
+
+
+const configurations = {
+  'de': deConfig,
+  'fr': frConfig,
+  'mx': mxConfig,
+};
+
+
+export {configurations};
--- src/index.js
+++ src/index.js
@@ -1,108 +1,72 @@
 import { MapComponent } from './MapComponent/MapComponent';
 
-import * as topojson from 'topojson';
+import {configurations} from './configurations';
 
 import styles from './app.css';
-import * as d3 from 'd3'
-
-const defaultConfig = {
-  mapUrl: 'http://localhost:8080/json/mex0.json',
-  mapDomElement: '.map',
-  toolTipDomElement: '.tooltip',
-  height: 768,
-  width: 1024,
-  projection: (width, height) => {
-    return d3
-    .geoMercator()
-    .scale(defaultConfig.scale)
-    .translate(config.initialPosition(width, height));
-  },
-  zoomEnabled: false,
-  scale: 3300,
-  featureExtractor: mapData => {
-    return topojson.feature(mapData, mapData.objects.state_data).features;
-  },
-
-  regionDisplayDataExtractor: regionData => {
-    return `${regionData.properties.NAME_1} <br/>`;
-  },
-  regionClassMapper: regionData => {
-    const regionName = regionData.properties.NAME_1;
-    return regionName.replace(/\s+/g, '_').toLowerCase();
-  },
-  initialPosition: (width, height) => {
-    return [width / 2, height / 2];
-  }
-};
-
-const deConfig = Object.assign({}, defaultConfig, {
-  mapUrl: 'http://localhost:8080/json/de0.json',
-  initialPosition: (width, height) => {
-    return [-60, height * 5];
-  }});
-
-const frConfig = Object.assign({}, defaultConfig, {
-  mapUrl: 'http://localhost:8080/json/fra0.json',
-  initialPosition: (width, height) => {
-    return [width / 2, height * 4.5];
-  }});
-
-const mxConfig = Object.assign({}, defaultConfig, {
-  mapUrl: 'http://localhost:8080/json/mex0.json',
-  scale: 2000,
-  projection: (width, height) => {
-    return d3
-      .geoAlbers()
-      .scale(mxConfig.scale)
-      .translate([width / 1.5, -100]);
-  }});
 
 
-const config = mxConfig;
+const changedMap = (event) => {
+  let countryCode = event.target.value;
+  loadMap(countryCode);
+}
 
+document.getElementById('map-selector').addEventListener('change', changedMap);
 
-fetch(config.mapUrl)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(mapData => {
-    const mapComponent = new MapComponent(config, mapData);
-    mapComponent.draw();
-    startListeningNotifications();
-  });
+const loadMap = (countryCode) => {
 
-
-let notificationTimerId;
+  let notificationTimerId;
 
 //TODO This can be controlled with a button
-const stopListeningNotifications = () => {
-  window.clearTimeout(notificationTimerId);
-}
-
-const startListeningNotifications = () => {
-  notificationTimerId = window.setTimeout(pickNotification, 2000);
-}
-
-const keepListeningNotifications = (currentNotification) => {
-  notificationTimerId = window.setTimeout(pickNotification, 5000, currentNotification);
-}
-
-
-const pickNotification = (previousNotification) => {
-  if (previousNotification) {
-    let pathName = document.getElementsByClassName(previousNotification.name)[0];
-    pathName.style.fill = 'green';
+  const stopListeningNotifications = () => {
+    if (notificationTimerId) {
+      window.clearTimeout(notificationTimerId);
+    }
   }
 
-  let currentNotification;
-  let hasNotificationsToProcess = notifications.length > 0
-  if (hasNotificationsToProcess) {
-    currentNotification = notifications.splice(0, 1)[0];
-    let pathName = document.getElementsByClassName(currentNotification.name)[0];
-    pathName.style.fill = 'pink';
+  const startListeningNotifications = () => {
+    notificationTimerId = window.setTimeout(pickNotification, 2000);
   }
-  keepListeningNotifications(currentNotification);
+
+  const keepListeningNotifications = (currentNotification) => {
+    notificationTimerId = window.setTimeout(pickNotification, 5000, currentNotification);
+  }
+
+  const pickNotification = (previousNotification) => {
+    if (previousNotification) {
+      let pathName = document.getElementsByClassName(previousNotification.name)[0];
+      pathName.style.fill = 'green';
+    }
+
+    let currentNotification;
+    let hasNotificationsToProcess = notifications.length > 0
+    if (hasNotificationsToProcess) {
+      currentNotification = notifications.splice(0, 1)[0];
+      let pathName = document.getElementsByClassName(currentNotification.name)[0];
+      pathName.style.fill = 'pink';
+    }
+    keepListeningNotifications(currentNotification);
+  }
+
+  const config = configurations[countryCode];
+
+  stopListeningNotifications();
+  document.getElementsByClassName(config.mapDomElement)[0].innerHTML = '';
+
+  fetch(config.mapUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(mapData => {
+      const mapComponent = new MapComponent(config, mapData);
+      mapComponent.draw();
+      // startListeningNotifications();
+    });
+
 }
+
+loadMap('mx');
+
+
 
 //TODO the notifications will come from a streaming source
 const deNotifications = [
